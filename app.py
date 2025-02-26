288eb4ebf08eac48848abacf14b93ccd2ea7615c
kravage <kostyash2009@mail.ru>
2025-02-26 17:07:55 +0300

    solution for third task

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def index(title):
 def training(prof):
     return render_template('training.html', prof=prof)
 
+@app.route('/list_prof/<param>', methods=['GET'])
+def prof_list(param):
+    query = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+             'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите',
+             'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода',
+             'киберинженер', 'штурман', 'пилот дронов']
+    return render_template('profession_list.html', param=param, query=query)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
