85a0d75295941ae2ba5fdb0147cf6bae38329f63
xvbxcnkasd <kostyash2009@mail.ru>
2025-03-26 18:01:09 +0300

    user api

--- data/users_api.py
+++ data/users_api.py
@@ -53,8 +53,8 @@ def create_user():
         speciality=request.json['speciality'],
         address=request.json['address'],
         email=request.json['email'],
-        hashed_password=request.json['hashed_password']
     )
+    user.set_password(request.json['hashed_password'])
     db_sess.add(user)
     db_sess.commit()
     return jsonify({'id': user.id})
@@ -63,20 +63,20 @@ def create_user():
 def edit_user(user_id):
     if not request.json:
         return make_response(jsonify({'error': 'Empty request'}, 400))
-    if not all(True for i in request.json if i in
-                                             ['surname', 'name', 'age', 'position', 'speciality', 'address', 'email']):
+    if not all([True if i in ['surname', 'name', 'age', 'position', 'speciality', 'address', 'email'] else False
+                for i in request.json]):
         return make_response(jsonify({'error': 'Bad request'}, 400))
     db_sess = db_session.create_session()
     user = db_sess.query(User).get(user_id)
     if not user:
         return make_response(jsonify({'error': 'User not found'}, 404))
-    user.surname = request.json['surname']
-    user.name = request.json['name']
-    user.age = request.json['age']
-    user.position = request.json['position']
-    user.speciality = request.json['speciality']
-    user.address = request.json['address']
-    user.email = request.json['email']
+    user.surname = request.json['surname'] if 'surname' in request.json.keys() else user.surname
+    user.name = request.json['name'] if 'name' in request.json.keys() else user.name
+    user.age = request.json['age'] if 'age' in request.json.keys() else user.age
+    user.position = request.json['position'] if 'position' in request.json.keys() else user.position
+    user.speciality = request.json['speciality'] if 'speciality' in request.json.keys() else user.speciality
+    user.address = request.json['address'] if 'address' in request.json.keys() else user.address
+    user.email = request.json['email'] if 'email' in request.json.keys() else user.email
     db_sess.commit()
     db_sess.close()
 
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from requests import *
 
+data = {'1': 1223, '2': 765}
 
-print(put('http://127.0.0.1:8080/api/users/1', json={'surname': 'eqr'}))
+print(data['2'] if '2' in data.keys() else '')
+print(put('http://127.0.0.1:8080/api/users/1', json={'surname': '123', 'name': '246'}))
